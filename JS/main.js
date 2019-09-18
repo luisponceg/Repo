670d29ea25c1f26d3ec8aad2987da45887a84722
luipo <l.ponce@feraz.com.mx>
2019-09-18 13:31:34 -0500

    ....

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,16 @@
+    /*alert("Hello World!");*/
+var nombre = "luis ponce";
+var altura = 170;
+var concat = nombre + " " +altura;   
+/*document.write(concat);*/
 
-    alert("Hola Mundo!");
+var datos = document.getElementById("datos");
+/*datos.innerHTML = concat;*/
+
+datos.innerHTML =
+      <h1>Soy caja de datos</h1>
+      <h2>Mi nombre es: $(nombre)</h2>
+      <h3>Mido: $(altura) cm</h3>
+;
+
 
