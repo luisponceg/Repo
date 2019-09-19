f31e7b774d959599cac109b5aa47bee6a7171228
luipo <l.ponce@feraz.com.mx>
2019-09-19 10:27:39 -0500

    ejemplo

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('Hola Mundo');
+alert("Hello World!");
+// console.log('Hola Mundo');
+ var nombre = "luis ponce";
+ var altura = 170;
+// var concat = nombre + " " +altura;   
+/*document.write(concat);*/
+
+// var datos = document.getElementById("datos");
+/*datos.innerHTML = concat;*/
+
+datos.innerHTML =
+`<h1> Soy caja de datos</h1>
+<h2>Mi nombre es: ${nombre}</h2>
+<h3>Mido: ${altura} cm</h3>`
+     
