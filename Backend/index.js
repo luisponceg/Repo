cba69d9e8c7247f179cbe7231cbe40a27eb4fc45
luipo <you@example.com>
2019-11-20 13:09:54 -0600

    ...

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,9 @@
 'use strict'
 
-var mongoose = require ('mongoose');
-
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.set('useFindAndModify',false)
 mongoose.connect('mongodb://localhost:27017/api_rest_blog',{useUnifiedTopology: true,useNewUrlParser:true,useCreateIndex: true})
 .then(()=> {
-    console.log('La conexion a la base de datos se ha realizado correcta!!!');
+    console.log('La conexion a la base de datos se ha realizado correctamente!!!');
 });
