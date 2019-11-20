6fc7d7a511de3de61234341d23e7d74b671840f8
luipo <you@example.com>
2019-11-20 12:46:49 -0600

    ...

--- Backend/index.js
+++ Backend/index.js
@@ -4,5 +4,5 @@ var mongoose = require ('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/api_rest_blog',{useNewUrlParser:true})
 .then(()=> {
-    console.log('La conexion a la base de datos se ha realizado bien')
+    console.log('La conexion a la base de datos se ha realizado correctamente')
 });
