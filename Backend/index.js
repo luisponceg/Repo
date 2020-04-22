3e2ab8dec51ffebde2b46568f39bfd4e91b673dc
luipo <you@example.com>
2020-04-22 16:44:45 -0500

    ...

--- Backend/index.js
+++ Backend/index.js
@@ -5,5 +5,5 @@ mongoose.Promise = global.Promise;
 mongoose.set('useFindAndModify',false)
 mongoose.connect('mongodb://localhost:27017/api_rest_blog',{useUnifiedTopology: true,useNewUrlParser:true,useCreateIndex: true})
 .then(()=> {
-    console.log('La conexion a la base de datos se ha realizado correctamente!!!');
+    console.log('La conexion a la base de datos se ha realizado bien!!!');
 });
