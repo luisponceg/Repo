64ef0206fcc81b06809bc0700dcbeaa2581ac8cd
luipo <you@example.com>
2020-04-23 18:50:32 -0500

    ...

--- Backend/routes/article.js
+++ Backend/routes/article.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var express = require ('express');
-var Aticlecontroller = require('../controllers/article');
+var AticleController = require('../controllers/article');
 
 var router= express.Router();
 
