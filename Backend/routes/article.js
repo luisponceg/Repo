f364b1a173596e8bf8e9437f7ba5dd812c4af122
luipo <you@example.com>
2021-02-16 09:57:53 -0600

    ...

--- Backend/routes/article.js
+++ Backend/routes/article.js
@@ -17,7 +17,7 @@ router.get('/articles/:last?',ArticleController.getArticles);
 router.get('/article/:id',ArticleController.getArticle);
 router.put('/article/:id',ArticleController.update);
 router.delete('/article/:id',ArticleController.delete);
-router.post('/upload-image/:id',md_upoad,ArticleController.upload);
+router.post('/upload-image/:id',md_upload,ArticleController.upload);
 
 
 module.exports = router;
