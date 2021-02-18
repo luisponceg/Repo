0808433399b26b95bbaa193c82fc2e1437ac1831
luipo <you@example.com>
2021-02-18 10:58:30 -0600

    ...

--- Backend/controllers/article.js
+++ Backend/controllers/article.js
@@ -282,6 +282,22 @@ var controller ={
         }           
        
     },//end upload file
+
+    getImage: (req, res) => {
+        var file = req.params.image;
+        var path_file = './upload/articles/'+file;
+
+        fs.exists(path_file, (exists) => {
+            if(exists){
+                return res.sendFile(path.resolve(path_file));
+            }else{
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'La imagen no existe !!!'
+                });
+            }
+        });
+    },
    
 };// end controller
 
