d7fb1b3437336b7ca40b2f993f3ee8d3b70faa17
luipo <you@example.com>
2021-02-19 11:04:08 -0600

    ...

---
+++ MasterReact/src/components/Micomponente.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Micomponente extends React.Component{
+    render(){
+        return {
+            <h1>Hola, soy el componente llamado: Micomponente<h1>
+        };
+    }
+}
