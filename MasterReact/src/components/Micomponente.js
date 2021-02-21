7d6d10c4d9040e3675c68c7f369ceff2d8a63c33
luipo <you@example.com>
2021-02-21 09:49:36 -0600

    ...

--- MasterReact/src/components/Micomponente.js
+++ MasterReact/src/components/Micomponente.js
@@ -2,6 +2,11 @@ import React,{Component} from 'react';
 
 class MiComponente extends React.Component{
     render(){
+        let receta = {
+            nombre: 'Pizza',
+            ingredientes: ['Tomate','Queso','Jamon'],
+            calorias: 400
+        }
         return (
             <div className='mi componente"'>
                <h1>Hola soy el componente: MiComponente</h1> 
