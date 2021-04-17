f028ca8079dbdb7b2740b40f1543aac80cd4981d
luisponceg <55158864+luisponceg@users.noreply.github.com>
2021-04-17 16:06:50 -0500

    ....

---
+++ MasterReact/src/components/Pelicula.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+
+class Pelicula extends Component {
+
+    marcar = ()  =>{
+        this.props.marcarFavorita(this.props.pelicula);
+    }
+    render (){
+        const {titulo,image} = this.props.pelicula;
+        return(
+            <article class="article-item" id="article-template">
+            <div class="image-wrap">
+                <img src={image} alt={titulo}/>
+            </div>
+           
+            <h2>{titulo}</h2>
+            <span class="date">
+                Hace 5 min
+            </span>
+            <a href="#">Leer mas</a>
+            <button onClick={this.marcar}>
+                Marcar como favorita
+            </button>
+            <div class="clearfix"></div>
+        </article>
+
+        )
+
+    };
+}
+
+export default Pelicula;
